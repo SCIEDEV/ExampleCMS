f20a54a80670e08354d775ea9fabaaba7b0a12af
Hardy Wen <hardyywwwwww@gmail.com>
2023-10-16 09:50:16 +0800

    Add Data Input Validation

--- Models/Classes.cs
+++ Models/Classes.cs
@@ -6,10 +6,15 @@ namespace CMS.Models
     public class Classes
     {
         [Key]
+
         public int ID { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Period is required.")]
+        [Range(1, 10, ErrorMessage = "Period must be between 1 and 10.")]
         public int period { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Class name is required.")]
+        [StringLength(100, ErrorMessage = "Class name can't be longer than 100 characters.")]
         public string className { get; set; }
     }
 }
